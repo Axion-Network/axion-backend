1478818ec46d6c40e6c100c2da60daf5ea3bf273
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2020-07-07 19:41:01 +0300

    database and message hash / signature signing

---
+++ hex2x_snapshot/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class HexUser(models.Model):
+    user_address = models.CharField(max_length=100)
+    hex_amount = models.DecimalField(max_digits=len(str(2**256)), decimal_places=0)
+    user_hash = models.CharField(max_length=100)
+    hash_signature = models.CharField(max_length=100)
