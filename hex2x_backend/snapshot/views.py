8c148b057b079957d074fc9f2e06767196e0d973
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2020-08-27 17:28:15 +0300

    return zero if user not found

--- hex2x_backend/snapshot/views.py
+++ hex2x_backend/snapshot/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.exceptions import NotFound, APIException
+from rest_framework.response import Response
 
 from .models import HexUser
 from .serializers import HexAddressSerializer
@@ -18,3 +20,14 @@ class HexAddressViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = HexAddressSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     pagination_class = HexAddressViewSetPagination
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            return super().retrieve(request, *args, **kwargs)
+        except:
+            serializer = HexAddressSerializer()
+            data = serializer.to_representation(kwargs)
+            data_dict = dict(data)
+            data_dict['hex_amount'] = 0
+            print(data_dict)
+            return Response(data_dict)
