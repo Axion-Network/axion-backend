eec534cd9f98d44c9fb6cd2ec97cbf163d54463c
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2020-11-05 03:33:31 +0300

    fix

--- hex2x_backend/tokenholders/models.py
+++ hex2x_backend/tokenholders/models.py
@@ -9,8 +9,13 @@ class TokenTransfer(models.Model):
     block_number = models.IntegerField(null=True, default=None)
     parsed = models.BooleanField(default=False)
 
-class TokenTransferHex2t(TokenTransfer):
-    pass
+class TokenTransfer(models.Model):
+    from_address = models.CharField(max_length=512)
+    to_address = models.CharField(max_length=512)
+    amount = models.DecimalField(max_digits=len(str(2**256)), decimal_places=0, null=True, default=None)
+    tx_hash = models.CharField(max_length=512, null=True, default=None)
+    block_number = models.IntegerField(null=True, default=None)
+    parsed = models.BooleanField(default=False)
 
 
 class TokenStakeStart(models.Model):
